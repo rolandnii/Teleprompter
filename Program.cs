b28223ac882c1fa5cc10dae0a0338628028f2397
rolandnii <rolandniidodoo@gmail.com>
2023-11-21 06:47:41 +0000

    initial

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace TeleprompterConsole
+{
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var lines = ReadFrom("Read.txt");
+			
+			foreach (var line in lines)
+			{
+				Console.Write(line);
+				
+				if (!string.IsNullOrWhiteSpace(line))
+				{
+					var pause = Task.Delay(500);
+					pause.Wait();
+				}
+			}
+		}
+
+		static IEnumerable<string> ReadFrom(string file)
+		{
+			string? line;
+			using (var reader = File.OpenText(file))
+			{
+
+				while ((line = reader.ReadLine()) != null)
+				{
+					var words = line.Split(" ");
+					foreach (var word in words)
+					{
+						yield return word + " ";
+					}
+
+					yield return Environment.NewLine;
+				}
+			}
+		}
+	}
+
+}
