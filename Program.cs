58d66aac193ff6923be153bbad9a97ed5f9f644e
rolandnii <rolandniidodoo@gmail.com>
2023-11-22 03:54:34 +0000

    add task to it

--- Program.cs
+++ Program.cs
@@ -1,42 +1,139 @@
-﻿namespace TeleprompterConsole
+﻿using static System.Math;
+namespace TeleprompterConsole
 {
 
-	internal class Program
+	 class Program
 	{
-		static void Main(string[] args)
+		
+		static async Task Main(string[] args)
 		{
-			var lines = ReadFrom("Read.txt");
-			
-			foreach (var line in lines)
+			try
 			{
-				Console.Write(line);
 				
-				if (!string.IsNullOrWhiteSpace(line))
+				await RunTeleprompter();
+
+			}
+			catch (Exception ex) {
+				Console.WriteLine(ex.Message);
+			}
+		}
+
+		private static async Task GetInput(TelePrompterConfig config)
+		{
+			
+			Action work = () =>
+			{
+                //do
+                //{
+                //                Console.WriteLine(1);
+                //                var key = Console.ReadKey();
+                //	if (key.KeyChar == '>')
+                //	{
+                //		config.UpdateDelay(20);
+                //	}
+
+                //	if (key.KeyChar == '<')
+                //	{
+                //		config.UpdateDelay(-20);
+
+                //	}
+                //	else if (key.KeyChar == 'X' || key.KeyChar == 'x')
+                //	{
+                //		config.SetDone();
+                //	}
+
+
+                //} while (!config.Done);
+				var num = 0;
+
+                while (num <100)
+                {
+                    Console.WriteLine('1');
+					num += 2;
+                }
+            };
+
+
+			await Task.Run(work);
+
+		}
+
+		private static async Task ShowTeleprompter(TelePrompterConfig config,string filename)
+		{
+			var words = ReadFrom(filename);
+			foreach (var word in words)
+			{
+				Console.Write(word);
+				if (!string.IsNullOrWhiteSpace(word))
 				{
-					var pause = Task.Delay(500);
-					pause.Wait();
+					await Task.Delay(config.DelayInMilliseconds);
 				}
 			}
+
+			config.SetDone();
 		}
 
 		static IEnumerable<string> ReadFrom(string file)
 		{
 			string? line;
+			var lineLength = 0;
 			using (var reader = File.OpenText(file))
 			{
 
 				while ((line = reader.ReadLine()) != null)
 				{
+
 					var words = line.Split(" ");
 					foreach (var word in words)
 					{
+
 						yield return word + " ";
+						lineLength += word.Length + 1;
+						if (lineLength > 75)
+						{
+							yield return Environment.NewLine;
+							lineLength = 0;
+						}
+
 					}
 
-					yield return Environment.NewLine;
 				}
 			}
 		}
+
+		private static async Task RunTeleprompter()
+		{
+			var config = new TelePrompterConfig();
+			var displayTask = ShowTeleprompter(config,"Read.txt");
+			var speedTask = GetInput(config);
+
+			await Task.WhenAll(displayTask, speedTask);
+	
+
+		}
+
+
+
 	}
+
+
+	//The config class
+	 class TelePrompterConfig
+	{
+		public int DelayInMilliseconds { get; private set; } = 250;
+
+		public void UpdateDelay(int increment)
+		{
+			var newDelay = Max(DelayInMilliseconds + increment, 1000);
+			newDelay = Min(newDelay, 20);
+			DelayInMilliseconds = newDelay;
+
+		}
+
+		public bool Done { get; private set; }
+
+		public void SetDone() { Done = true; }
+	}
+
 
 }
